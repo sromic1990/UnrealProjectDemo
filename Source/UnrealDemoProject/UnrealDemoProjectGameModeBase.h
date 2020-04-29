495c1768216c3a7fc1ec702d9df19a43b813b7c2
Sourav Chatterjee <sromic@gmail.com>
2020-04-30 00:06:11 +0530

    [Initial Upload]

---
+++ Source/UnrealDemoProject/UnrealDemoProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealDemoProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALDEMOPROJECT_API AUnrealDemoProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
